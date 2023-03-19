0570cf819921ec55d966bf34f0a078bcbc4edb40
Sergi Mus <sergi.mus@gmail.com>
2023-03-19 13:31:38 +0100

    Added command execution

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,16 @@ fn main() {
     println!("Reading configuration from {}", args.config_path);
     let config = Config::read(&args.config_path);
     println!("Configuration: {:?}", config);
+    match std::fs::read_dir(&config.job_path) {
+        Ok(job_paths) => {
+            for path in job_paths {
+                let job = Job::read(path.unwrap().path().to_str().unwrap());
+                println!("Found job: {:?}", job);
+                job.run();
+            }
+        }
+        Err(err) => println!("Job dir not found: {}", err)
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -27,5 +37,27 @@ impl Config {
     fn read(path: &str) -> Self {
         let file = std::fs::File::open(path).unwrap();
         serde_json::from_reader(file).unwrap()
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Job {
+    command: String,
+    args: Vec<String>,
+}
+
+impl Job {
+    fn read(path: &str) -> Self {
+        let file = std::fs::File::open(path).unwrap();
+        serde_json::from_reader(file).unwrap()
+    }
+
+    fn run(&self) {
+        let mut cmd = std::process::Command::new(&self.command);
+        for arg in self.args.iter() {
+            cmd.arg(arg);
+        }
+        let output = cmd.output().unwrap();
+        println!("{:?}", output);
     }
 }
