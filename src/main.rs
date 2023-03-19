8abb8e601f43b594f26f6771eaa3f3477fafd23b
musergi <sergi.mus@gmail.com>
2023-03-19 09:17:51 +0100

    Added config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-struct Config {
-    data_path: String,
-    job_path: String,
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+/// Osprei CI server
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File path to the configuration
+    #[arg(short, long)]
+    config_path: String,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Reading configuration from {}", args.config_path);
+    let config = Config::read(&args.config_path);
+    println!("Configuration: {:?}", config);
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Config {
+    job_path: String,
+    data_path: String,
+}
+
+impl Config {
+    fn read(path: &str) -> Self {
+        let file = std::fs::File::open(path).unwrap();
+        serde_json::from_reader(file).unwrap()
+    }
 }
