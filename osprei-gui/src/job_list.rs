7bd2c8f90328c2ae942e9b05b4301ef83304fbdf
musergi <sergi.mus@gmail.com>
2023-04-20 20:29:10 +0200

    Added job list

---
+++ osprei-gui/src/job_list.rs
@@ -0,0 +1,17 @@
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub jobs: Vec<String>,
+}
+
+#[function_component]
+pub fn JobList(props: &Props) -> Html {
+    html! {
+        <ul>
+            {
+                props.jobs.clone().into_iter().map(|job| html!{<li>{ job }</li>}).collect::<Html>()
+            }
+        </ul>
+    }
+}
--- osprei-gui/src/lib.rs
+++ osprei-gui/src/lib.rs
@@ -1,10 +1,35 @@
+use std::ops::Deref;
+
+use gloo_net::http::Request;
 use yew::prelude::*;
 
+mod job_list;
 mod missing;
 
 #[function_component]
 pub fn App() -> Html {
+    let jobs = use_state(|| vec![]);
+    {
+        let jobs = jobs.clone();
+        use_effect_with_deps(
+            move |_| {
+                let jobs = jobs.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_jobs: Vec<String> = Request::get("http://localhost:10000/job")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    jobs.set(fetched_jobs);
+                });
+                || ()
+            },
+            (),
+        );
+    }
     html! {
-        <missing::Missing/>
+        <job_list::JobList jobs={ jobs.deref().clone() } />
     }
 }
