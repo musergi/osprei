4e46bc017075881e1339400b7defc9ce8400aaea
musergi <sergi.mus@gmail.com>
2023-04-29 22:46:44 +0200

    Added cards for jobs

--- osprei-gui/src/job_list.rs
+++ osprei-gui/src/job_list.rs
@@ -7,16 +7,38 @@ pub struct Props {
 
 #[function_component]
 pub fn JobList(props: &Props) -> Html {
+    let cards: Html = props
+        .jobs
+        .iter()
+        .map(|job_name| html!{
+            <JobCard job_name={job_name.clone()} job_status={"Not executed"}/>
+        })
+        .collect();
     html! {
         <ul>
-            {
-                props
-                    .jobs
-                    .clone()
-                    .into_iter()
-                    .map(|job| html!{ <li>{ job }</li> })
-                    .collect::<Html>()
-            }
+            {cards}
         </ul>
     }
 }
+
+#[derive(Properties, PartialEq)]
+struct CardProps {
+    job_name: String,
+    job_status: String,
+}
+
+#[function_component]
+fn JobCard(props: &CardProps) -> Html {
+    let CardProps {job_name, job_status} = props;
+    let job_name: String = job_name.to_uppercase().chars().map(|c| match c {
+        '_' => ' ',
+        c => c,
+    }).collect();
+    html! {
+        <li class={"job-card"}>
+            <h3>{job_name}</h3>
+            <p>{job_status}</p>
+        </li>
+    }
+}
+
