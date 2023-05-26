fd015469ebc9824c6631d49d9f42826ca4786688
musergi <sergi.mus@gmail.com>
2023-05-26 08:23:04 +0200

    Added port to leptos

--- osprei-gui/src/lib.rs
+++ osprei-gui/src/lib.rs
@@ -1,35 +1,74 @@
-use std::ops::Deref;
+use leptos::{ev::SubmitEvent, html::Input, *};
+use log::info;
 
-use gloo_net::http::Request;
-use yew::prelude::*;
+pub fn osprei_gui(cx: Scope) -> impl IntoView {
+    let (is_adding, set_is_adding) = create_signal(cx, false);
+    let jobs = create_resource(cx, || (), load_jobs);
 
-mod job_list;
-mod missing;
+    let last = move || match is_adding() {
+        true => {
+            let name_ref: NodeRef<Input> = create_node_ref(cx);
+            let source_ref: NodeRef<Input> = create_node_ref(cx);
+            let path_ref: NodeRef<Input> = create_node_ref(cx);
 
-#[function_component]
-pub fn App() -> Html {
-    let jobs = use_state(|| vec![]);
-    {
-        let jobs = jobs.clone();
-        use_effect_with_deps(
-            move |_| {
-                let jobs = jobs.clone();
-                wasm_bindgen_futures::spawn_local(async move {
-                    let fetched_jobs: Vec<String> = Request::get("http://localhost:10000/job")
-                        .send()
-                        .await
-                        .unwrap()
-                        .json()
-                        .await
-                        .unwrap();
-                    jobs.set(fetched_jobs);
-                });
-                || ()
-            },
-            (),
-        );
+            let on_submit = move |ev: SubmitEvent| {
+                ev.prevent_default();
+
+                let name = name_ref().unwrap().value();
+                let source = source_ref().unwrap().value();
+                let path = path_ref().unwrap().value();
+
+                set_is_adding.set(false);
+                info!("Submitted {}, {}, {}", name, source, path);
+            };
+
+            view! { cx,
+                <li>
+                    <form on:submit=on_submit>
+                        <input type="text" placeholder="name" ref={ name_ref } />
+                        <input type="text" placeholder="source" ref={ source_ref } />
+                        <input type="text" placeholder="path" ref={ path_ref } />
+                        <button>"Add"</button>
+                    </form>
+                </li>
+            }
+        }
+        false => view! { cx,
+            <li>
+                <button on:click=move |_| { set_is_adding.set(true) }>"New"</button>
+            </li>
+        },
+    };
+
+    view! { cx,
+        <ul>
+            <li>"Job 1"</li>
+            <li>"Job 2"</li>
+            { last }
+        </ul>
     }
-    html! {
-        <job_list::JobList jobs={ jobs.deref().clone() } />
+}
+
+async fn load_jobs(_: ()) -> Vec<osprei::JobPointer> {
+    let url = "http://localhost:10000/job";
+    let job_ids: Vec<i64> = reqwasm::http::Request::get(url)
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+    let mut jobs = Vec::new();
+    for job_id in job_ids {
+        let url = format!("http://localhost:10000/job/{}", job_id);
+        let job = reqwasm::http::Request::get(&url)
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+        jobs.push(job);
     }
+    jobs
 }
--- osprei-gui/src/main.rs
+++ osprei-gui/src/main.rs
@@ -1,6 +1,6 @@
-use osprei_gui::App;
+use leptos::mount_to_body;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::Renderer::<App>::new().render();
+    mount_to_body(osprei_gui::osprei_gui)
 }
