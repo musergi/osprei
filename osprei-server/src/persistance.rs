d637814ed0b048eadd14277128ff427e664a9bd5
musergi <sergi.mus@gmail.com>
2023-07-23 17:23:02 +0200

    Remove dead code

--- osprei-server/src/persistance.rs
+++ osprei-server/src/persistance.rs
@@ -1,28 +1,5 @@
 use warp::Filter;
 
-#[async_trait::async_trait]
-pub trait JobStore {
-    async fn list_jobs(&self) -> Vec<i64>;
-    async fn store_job(&self, name: String, source: String, path: String) -> i64;
-    async fn fetch_job(&self, id: i64) -> osprei::JobPointer;
-    async fn fetch_job_description(&self, id: i64) -> osprei::Job;
-}
-
-#[async_trait::async_trait]
-pub trait ExecutionStore {
-    async fn create_execution(&self, job_id: i64) -> i64;
-    async fn set_execution_status(&self, id: i64, execution_status: osprei::ExecutionStatus);
-    async fn get_execution(&self, id: i64) -> osprei::ExecutionDetails;
-    async fn last_executions(&self, job_id: i64, limit: usize) -> Vec<osprei::ExecutionSummary>;
-}
-
-#[async_trait::async_trait]
-pub trait ScheduleStore {
-    async fn create_daily(&self, job_id: i64, request: osprei::ScheduleRequest) -> i64;
-    async fn get_schedules(&self, job_id: i64) -> Vec<osprei::Schedule>;
-    async fn get_all_schedules(&self) -> Vec<osprei::Schedule>;
-}
-
 type StoreResult<T> = Result<T, StorageError>;
 
 #[async_trait::async_trait]
@@ -196,8 +173,6 @@ pub enum StorageError {
     InternalError(String),
 }
 
-pub trait Store: JobStore + ExecutionStore + ScheduleStore + Send + Sync + 'static {}
-
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "type")]
 pub enum PersistanceConfig {
@@ -237,8 +212,6 @@ pub fn with(
 
 #[cfg(test)]
 mod test {
-    use osprei::ScheduleRequest;
-
     use super::StorageError;
 
     pub async fn listed_jobs_increase_on_job_added(storage: impl super::Storage) {
@@ -321,71 +294,6 @@ mod test {
             execution.status,
             Some(osprei::ExecutionStatus::Success)
         ));
-    }
-
-    pub async fn test_job_store<T: super::JobStore>(store: T) {
-        let job_ids = store.list_jobs().await;
-        let initial_job_count = job_ids.len();
-
-        let name = String::from("test_job_name");
-        let source = String::from("https://github.com/musergi/osprei.git");
-        let path = String::from(".ci/test.json");
-        let stored_id = store.store_job(name.clone(), source, path).await;
-
-        let job_ids = store.list_jobs().await;
-        assert_eq!(job_ids.len(), initial_job_count + 1);
-
-        let fetched = store.fetch_job(stored_id).await;
-        assert_eq!(&fetched.id, &stored_id);
-        assert_eq!(&fetched.name, &name);
-    }
-
-    pub async fn test_execution_store<T: super::ExecutionStore + super::JobStore>(store: T) {
-        let name = String::from("test_job_name");
-        let source = String::from("https://github.com/musergi/osprei.git");
-        let path = String::from(".ci/test.json");
-        let job_id = store.store_job(name.clone(), source, path).await;
-
-        let execution_id = store.create_execution(job_id).await;
-        let recent_executions = store.last_executions(job_id, 1).await;
-        let most_recent_execution = recent_executions.get(0).unwrap();
-        assert_eq!(most_recent_execution.id, execution_id);
-
-        let execution = store.get_execution(execution_id).await;
-        assert_eq!(execution.job_name, name);
-        assert!(execution.status.is_none());
-
-        store
-            .set_execution_status(execution_id, osprei::ExecutionStatus::Success)
-            .await;
-        let execution = store.get_execution(execution_id).await;
-        assert_eq!(execution.status, Some(osprei::ExecutionStatus::Success));
-    }
-
-    pub async fn test_schedule_store<T: super::ScheduleStore + super::JobStore>(store: T) {
-        let name = String::from("test_job_name");
-        let source = String::from("https://github.com/musergi/osprei.git");
-        let path = String::from(".ci/test.json");
-        let job_id = store.store_job(name.clone(), source, path).await;
-
-        let schedule_id = store
-            .create_daily(
-                job_id,
-                ScheduleRequest {
-                    hour: 12,
-                    minute: 0,
-                },
-            )
-            .await;
-        let schedules = store.get_schedules(job_id).await;
-        assert_eq!(schedules.len(), 1);
-        let schedule = schedules.get(0).unwrap();
-        assert_eq!(schedule.schedule_id, schedule_id);
-        assert_eq!(schedule.job_id, job_id);
-        assert_eq!(schedule.hour, 12);
-        assert_eq!(schedule.minute, 0);
-
-        assert_eq!(store.get_all_schedules().await.len(), 1);
     }
 }
 
--- osprei-server/src/persistance/sqlite.rs
+++ osprei-server/src/persistance/sqlite.rs
@@ -1,8 +1,8 @@
 use log::error;
-use osprei::{ExecutionDetails, ExecutionSummary, Job, JobPointer};
+use osprei::{ExecutionDetails, ExecutionSummary, JobPointer};
 use sqlx::Row;
 
-use super::{ExecutionStore, JobStore, ScheduleStore, Storage, StorageError, Store, StoreResult};
+use super::{Storage, StorageError, StoreResult};
 
 #[derive(Debug, Clone)]
 pub struct DatabasePersistance {
@@ -36,160 +36,6 @@ impl DatabasePersistance {
         DatabasePersistance { pool }
     }
 }
-
-#[async_trait::async_trait]
-impl JobStore for DatabasePersistance {
-    async fn list_jobs(&self) -> Vec<i64> {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT id FROM jobs")
-            .fetch_all(&mut conn)
-            .await
-            .unwrap()
-            .into_iter()
-            .map(|row| row.get(0))
-            .collect()
-    }
-
-    async fn store_job(&self, name: String, source: String, path: String) -> i64 {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("INSERT INTO jobs (name, source, path) VALUES ($1, $2, $3)")
-            .bind(name)
-            .bind(source)
-            .bind(path)
-            .execute(&mut conn)
-            .await
-            .unwrap()
-            .last_insert_rowid()
-    }
-
-    async fn fetch_job(&self, id: i64) -> JobPointer {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT id, name, source, path FROM jobs WHERE id = $1")
-            .bind(id)
-            .fetch_one(&mut conn)
-            .await
-            .map(|row| JobPointer {
-                id: row.get(0),
-                name: row.get(1),
-                source: row.get(2),
-                path: row.get(3),
-            })
-            .unwrap()
-    }
-
-    async fn fetch_job_description(&self, _id: i64) -> Job {
-        todo!()
-    }
-}
-
-#[async_trait::async_trait]
-impl ExecutionStore for DatabasePersistance {
-    async fn create_execution(&self, job_id: i64) -> i64 {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("INSERT INTO executions (job_id, start_time) VALUES ($1, CURRENT_TIMESTAMP)")
-            .bind(job_id)
-            .execute(&mut conn)
-            .await
-            .unwrap()
-            .last_insert_rowid()
-    }
-
-    async fn set_execution_status(&self, id: i64, execution_status: osprei::ExecutionStatus) {
-        let value: i64 = execution_status.into();
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("UPDATE executions SET status = $2 WHERE id = $1;")
-            .bind(id)
-            .bind(value)
-            .execute(&mut conn)
-            .await
-            .unwrap();
-    }
-
-    async fn get_execution(&self, id: i64) -> ExecutionDetails {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT executions.id, jobs.name, start_time, status FROM executions JOIN jobs ON jobs.id = executions.job_id WHERE executions.id = $1")
-            .bind(id)
-            .fetch_one(&mut conn)
-            .await
-            .map(|row| {
-                let status_encoded: Option<i64> = row.get(3);
-                let status = status_encoded.map(osprei::ExecutionStatus::from);
-                ExecutionDetails {
-                    execution_id: row.get(0),
-                    job_name: row.get(1),
-                    start_time: row.get(2),
-                    status
-                }
-            })
-            .unwrap()
-    }
-
-    async fn last_executions(&self, job_id: i64, limit: usize) -> Vec<ExecutionSummary> {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT id, start_time FROM executions WHERE job_id = $1 ORDER BY start_time DESC LIMIT $2")
-            .bind(job_id)
-            .bind(limit as i64)
-            .fetch_all(&mut conn)
-            .await
-            .unwrap()
-            .into_iter()
-            .map(|row| ExecutionSummary {
-                id: row.get(0),
-                start_time: row.get(1)
-            })
-            .collect()
-    }
-}
-
-#[async_trait::async_trait]
-impl ScheduleStore for DatabasePersistance {
-    async fn create_daily(&self, job_id: i64, request: osprei::ScheduleRequest) -> i64 {
-        let osprei::ScheduleRequest { hour, minute } = request;
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("INSERT INTO schedules (job_id, hour, minute) VALUES ($1, $2, $3)")
-            .bind(job_id)
-            .bind(hour)
-            .bind(minute)
-            .execute(&mut conn)
-            .await
-            .unwrap()
-            .last_insert_rowid()
-    }
-
-    async fn get_schedules(&self, job_id: i64) -> Vec<osprei::Schedule> {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT id, job_id, hour, minute FROM schedules WHERE job_id = $1")
-            .bind(job_id)
-            .fetch_all(&mut conn)
-            .await
-            .unwrap()
-            .into_iter()
-            .map(|row| osprei::Schedule {
-                schedule_id: row.get(0),
-                job_id: row.get(1),
-                hour: row.get(2),
-                minute: row.get(3),
-            })
-            .collect()
-    }
-    async fn get_all_schedules(&self) -> Vec<osprei::Schedule> {
-        let mut conn = self.pool.acquire().await.unwrap();
-        sqlx::query("SELECT id, job_id, hour, minute FROM schedules")
-            .fetch_all(&mut conn)
-            .await
-            .unwrap()
-            .into_iter()
-            .map(|row| osprei::Schedule {
-                schedule_id: row.get(0),
-                job_id: row.get(1),
-                hour: row.get(2),
-                minute: row.get(3),
-            })
-            .collect()
-    }
-}
-
-impl Store for DatabasePersistance {}
 
 #[async_trait::async_trait]
 impl Storage for DatabasePersistance {
@@ -363,24 +209,6 @@ impl From<sqlx::Error> for StorageError {
 mod test {
     use super::*;
     use crate::persistance::test;
-
-    #[tokio::test]
-    async fn test_sqlite_job_store() {
-        let store = DatabasePersistance::new(":memory:").await;
-        test::test_job_store(store).await;
-    }
-
-    #[tokio::test]
-    async fn test_sqlite_execution_store() {
-        let store = DatabasePersistance::new(":memory:").await;
-        test::test_execution_store(store).await;
-    }
-
-    #[tokio::test]
-    async fn test_sqlite_schedule_store() {
-        let store = DatabasePersistance::new(":memory:").await;
-        test::test_schedule_store(store).await;
-    }
 
     #[tokio::test]
     async fn listed_jobs_increase_on_job_added() {
