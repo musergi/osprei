5b14e2d0f1aa24f2ef55fb6701a99706d9f22af9
musergi <sergi.mus@gmail.com>
2023-10-09 22:15:43 +0200

    Move execution flow to storage

--- osprei-server/src/views.rs
+++ osprei-server/src/views.rs
@@ -48,29 +48,8 @@ pub async fn get_job_run(
     pool: sqlx::SqlitePool,
     docker: Docker,
 ) -> Result<impl warp::Reply, Infallible> {
-    let mut conn = pool.acquire().await.unwrap();
-    let pointer = sqlx::query("SELECT id, name, definition FROM jobs WHERE id = $1")
-        .bind(job_id)
-        .fetch_optional(&mut conn)
-        .await
-        .unwrap()
-        .map(|row| {
-            let definition: String = row.get(2);
-            let definition = serde_json::from_str(&definition).unwrap();
-            osprei::JobPointer {
-                id: row.get(0),
-                name: row.get(1),
-                definition,
-            }
-        })
-        .unwrap();
-    let id =
-        sqlx::query("INSERT INTO executions (job_id, start_time) VALUES ($1, CURRENT_TIMESTAMP)")
-            .bind(job_id)
-            .execute(&mut conn)
-            .await
-            .unwrap()
-            .last_insert_rowid();
+    let storage = Storage::new(pool);
+    let (id, pointer) = storage.create_execution(job_id).await.unwrap();
     {
         let execution_id = id.clone();
         tokio::spawn(async move {
@@ -119,16 +98,10 @@ pub async fn get_job_run(
             }
             let stdout = String::from_utf8_lossy(&stdout).to_string();
             let stderr = String::from_utf8_lossy(&stderr).to_string();
-            sqlx::query(
-                "UPDATE executions SET status = $2, stdout = $3, stderr = $4 WHERE id = $1",
-            )
-            .bind(execution_id)
-            .bind(status)
-            .bind(stdout)
-            .bind(stderr)
-            .execute(&mut conn)
-            .await
-            .unwrap();
+            storage
+                .update_execution(execution_id, status, stdout, stderr)
+                .await
+                .unwrap();
             let opts = ContainerRemoveOpts::builder()
                 .force(true)
                 .volumes(true)
--- osprei-server/src/views/storage.rs
+++ osprei-server/src/views/storage.rs
@@ -71,6 +71,35 @@ const GET_JOB_QUERY: &str = "
         WHERE id = $1
     ";
 
+const INSERT_JOB_QUERY: &str = "
+    INSERT INTO jobs (
+        name,
+        definition
+    ) VALUES (
+        $1,
+        $2
+    )
+";
+
+const INSERT_EXECUTION_QUERY: &str = "
+    INSERT INTO executions (
+        job_id,
+        start_time
+    ) VALUES (
+        $1,
+        CURRENT_TIMESTAMP
+    )
+";
+
+const UPDATE_EXECUTION_QUERY: &str = "
+    UPDATE executions
+    SET
+        status = $2,
+        stdout = $3,
+        stderr = $4
+    WHERE id = $1
+";
+
 mod adapter;
 
 impl Storage {
@@ -114,12 +143,41 @@ impl Storage {
         let osprei::JobCreationRequest { name, definition } = request;
         let definition = serde_json::to_string(&definition).unwrap();
         let mut conn = self.pool.acquire().await?;
-        let id = sqlx::query("INSERT INTO jobs (name, definition) VALUES ($1, $2)")
+        let id = sqlx::query(INSERT_JOB_QUERY)
             .bind(name)
             .bind(definition)
             .execute(&mut conn)
             .await?
             .last_insert_rowid();
         Ok(id)
+    }
+
+    pub async fn create_execution(&self, job_id: i64) -> Result<(i64, osprei::JobPointer), Error> {
+        let pointer = self.get_job(job_id).await?;
+        let mut conn = self.pool.acquire().await?;
+        let id = sqlx::query(INSERT_EXECUTION_QUERY)
+            .bind(job_id)
+            .execute(&mut conn)
+            .await?
+            .last_insert_rowid();
+        Ok((id, pointer))
+    }
+
+    pub async fn update_execution(
+        &self,
+        execution_id: i64,
+        status: i64,
+        stdout: String,
+        stderr: String,
+    ) -> Result<(), Error> {
+        let mut conn = self.pool.acquire().await?;
+        sqlx::query(UPDATE_EXECUTION_QUERY)
+            .bind(execution_id)
+            .bind(status)
+            .bind(stdout)
+            .bind(stderr)
+            .execute(&mut conn)
+            .await?;
+        Ok(())
     }
 }
