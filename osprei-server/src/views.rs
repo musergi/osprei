d4cdb9bea1ad123d0c783751dfd018ba01bb9404
musergi <sergi.mus@gmail.com>
2023-10-10 20:35:06 +0200

    Refactor

--- osprei-server/src/views.rs
+++ osprei-server/src/views.rs
@@ -1,19 +1,14 @@
-use docker_api::conn::TtyChunk;
-use docker_api::opts::ContainerCreateOpts;
-use docker_api::opts::ContainerRemoveOpts;
-use docker_api::opts::LogsOpts;
-use docker_api::Containers;
 use docker_api::Docker;
-use osprei::Job;
 use osprei::JobCreationRequest;
-use osprei::JobPointer;
 use sqlx::Row;
 use std::convert::Infallible;
-use tokio_stream::StreamExt;
 use warp::reply::Reply;
 
 mod storage;
 use storage::Storage;
+
+mod execution;
+use execution::dispatch_execution;
 
 fn reply<S: serde::Serialize>(result: Result<S, storage::Error>) -> impl warp::Reply {
     match result {
@@ -48,68 +43,20 @@ pub async fn get_job_run(
     pool: sqlx::SqlitePool,
     docker: Docker,
 ) -> Result<impl warp::Reply, Infallible> {
+    let execution_id = job_run(job_id, pool, docker).await;
+    let reply = reply(execution_id);
+    Ok(reply)
+}
+
+async fn job_run(
+    job_id: i64,
+    pool: sqlx::SqlitePool,
+    docker: Docker,
+) -> Result<impl serde::Serialize, storage::Error> {
     let storage = Storage::new(pool);
     let (id, pointer) = storage.create_execution(job_id).await.unwrap();
-    {
-        let execution_id = id.clone();
-        tokio::spawn(async move {
-            let JobPointer {
-                name, definition, ..
-            } = pointer;
-            let Job {
-                source,
-                image,
-                command,
-                arguments,
-                ..
-            } = definition;
-            let checkout_path = format!("/opt/osprei/var/workspace/{}", name);
-            tokio::process::Command::new("git")
-                .arg("clone")
-                .arg(source)
-                .arg(&checkout_path)
-                .output()
-                .await
-                .unwrap();
-            let volume = format!("{v}:{v}", v = checkout_path);
-            let mut command = vec![command];
-            command.extend(arguments);
-            let opts = ContainerCreateOpts::builder()
-                .name(&name)
-                .image(image)
-                .working_dir(&checkout_path)
-                .command(command)
-                .volumes(vec![volume])
-                .build();
-            let container = Containers::new(docker).create(&opts).await.unwrap();
-            container.start().await.unwrap();
-            let status = container.wait().await.unwrap().status_code;
-            let status = if status == 0 { 0 } else { 1 };
-            let opts = LogsOpts::builder().stdout(true).stderr(true).build();
-            let mut stream = container.logs(&opts);
-            let mut stdout: Vec<u8> = Vec::new();
-            let mut stderr: Vec<u8> = Vec::new();
-            while let Some(text) = stream.next().await {
-                match text.unwrap() {
-                    TtyChunk::StdOut(out) => stdout.extend(out),
-                    TtyChunk::StdErr(out) => stderr.extend(out),
-                    _ => (),
-                }
-            }
-            let stdout = String::from_utf8_lossy(&stdout).to_string();
-            let stderr = String::from_utf8_lossy(&stderr).to_string();
-            storage
-                .update_execution(execution_id, status, stdout, stderr)
-                .await
-                .unwrap();
-            let opts = ContainerRemoveOpts::builder()
-                .force(true)
-                .volumes(true)
-                .build();
-            container.remove(&opts).await.unwrap();
-        });
-    }
-    Ok(warp::reply::json(&id))
+    dispatch_execution(id, pointer, storage, docker);
+    Ok(id)
 }
 
 pub async fn get_execution(
